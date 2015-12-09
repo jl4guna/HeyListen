d9b3486cc7d171687194a1ceb23226bb1a926834
LagunaJS <jhonlpdev@gmail.com>
2015-12-08 18:34:47 -0700

    Validacion de formularios mejorada

--- js/registro.js
+++ js/registro.js
@@ -1,64 +1,62 @@
 function Registrar() {
 	var url = 'http://heylisten20151203051142.azurewebsites.net/api/usuarios';
+	var formulario = {};
+	formulario.username = $('#username').val();
+	formulario.password = $('#password').val();
+	formulario.email = $('#email').val();
+	var data = JSON.stringify(formulario);
 	$.ajax({
-			url: url,
-			type: 'POST',
-			data: JSON.stringify($('#formulario').serializeObject()),
-			contentType: "application/json;chartset=utf-8",
-			statusCode: {
-					201: function () {
-							window.location.href = "index.html";
-					},
-					400: function () {
-							alertify.error("Ocurrio un error");
-					}
+		url: url,
+		type: 'POST',
+		data: data,
+		contentType: "application/json;chartset=utf-8",
+		statusCode: {
+			201: function () {
+				window.location.href = "index.html";
+			},
+			400: function () {
+				alertify.error("Ocurrio un error");
 			}
+		}
 	});
 }
 
 $.fn.serializeObject = function()
 {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
+	var o = {};
+	var a = this.serializeArray();
+	$.each(a, function() {
+		if (o[this.name] !== undefined) {
+			if (!o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || '');
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
 };
 
-$('#btnRegistrar').click(function(event) {
+
+//Expresion regular para comprobar el email
+var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+$("#btnRegistrar").click(function (event){
 	event.preventDefault();
-	if ($('#username').val() != '' && $('#email').val() != '' && $('#password').val() != '') {
-		if ($('#password').val() === $('#password2').val()) {
-			Registrar();
-		}else {
-			alertify.error("Las contraseñas no coinciden");
-		}
+	//Comprueba que el campo nombre no este vacio
+	if( $("#username").val() == "" ){
+		$("#username").focus().after(alertify.error("Ingresa un nombre de usuario"));
+		return false;
+	}else if( $("#email").val() == "" || !emailreg.test($("#email").val()) ){ //comprueba que el campo email no este vacio y sea valido
+		$("#email").focus().after(alertify.error("Ingresa un correo valido"));
+		return false;
+	}else if( $("#password").val() == ""){ //comprueba que el campo contraseña no este vacio
+		$("#password").focus().after(alertify.error("Ingresa una contraseña"));
+		return false;
+	}else if ($('#password').val() != $('#password2').val()) { //comprueba que las contraseñas existan
+		$("#password2").focus().after(alertify.error("Las contraseñas no coinciden"));
+		return false;
 	}
-
-});
-
-$(document).ready(function () {
-    var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
-    $("#btnRegistrar").click(function (){
-        $(".error").remove();
-        if( $("#username").val() == "" ){
-            $("#username").focus().after(alertify.error("Ingresa un nombre de usuario"));
-            return false;
-        }else if( $("#email").val() == "" || !emailreg.test($("#email").val()) ){
-            $("#email").focus().after(alertify.error("Ingresa un correo valido"));
-            return false;
-        }else if( $("#password").val() == ""){
-            $("#password").focus().after(alertify.error("Ingresa una contraseña"));
-            return false;
-        }
-    });
+	Registrar();
 
 });
--- js/usuario.js
+++ js/usuario.js
@@ -8,18 +8,18 @@ function Actualizar() {
 	formulario.email = $('#email').val();
 	var data = JSON.stringify(formulario);
 	$.ajax({
-			url: url,
-			type: 'PUT',
-			data: data,
-			contentType: "application/json;chartset=utf-8",
-			statusCode: {
-					204: function () {
-							alertify.success("Informacion Actualizada");
-					},
-					400: function () {
-							alertify.error("Ocurrio un error");
-					}
+		url: url,
+		type: 'PUT',
+		data: data,
+		contentType: "application/json;chartset=utf-8",
+		statusCode: {
+			204: function () {
+				alertify.success("Informacion Actualizada");
+			},
+			400: function () {
+				alertify.error("Ocurrio un error");
 			}
+		}
 	});
 }
 
@@ -27,63 +27,55 @@ function  Cargar() {
 	var id = localStorage["usuario"];
 	var url = 'http://heylisten20151203051142.azurewebsites.net/api/usuarios/'+id;
 	$.ajax({
-			url: url,
-			type: 'GET',
-			contentType: "application/json;chartset=utf-8",
-			success: function(usuario){
-				$('#username').val(usuario.username);
-				$('#password').val(usuario.password);
-				$('#email').val(usuario.email);
-      },
+		url: url,
+		type: 'GET',
+		contentType: "application/json;chartset=utf-8",
+		success: function(usuario){
+			$('#username').val(usuario.username);
+			$('#password').val(usuario.password);
+			$('#email').val(usuario.email);
+		},
 	});
 }
 
 $.fn.serializeObject = function()
 {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
+	var o = {};
+	var a = this.serializeArray();
+	$.each(a, function() {
+		if (o[this.name] !== undefined) {
+			if (!o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || '');
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
 };
 
-$('#btnActualizar').click(function(event) {
-	var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+
+var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+$("#btnActualizar").click(function (event){
 	event.preventDefault();
-	if ($('#username').val() != '' && $('#email').val() != '' && $('#password').val() != '' && emailreg.test($("#email").val())) {
-		if ($('#password').val() && $('#password2').val()) {
-				Actualizar();
-		}
+	if( $("#username").val() == "" ){
+		$("#username").focus().after(alertify.error("Ingresa un nombre de usuario"));
+		return false;
+	}else if( $("#email").val() == "" || !emailreg.test($("#email").val()) ){
+		$("#email").focus().after(alertify.error("Ingresa un correo valido"));
+		return false;
+	}else if( $("#password").val() == ""){
+		$("#password").focus().after(alertify.error("Ingresa una contraseña"));
+		return false;
+	}else if ($('#password').val() != $('#password2').val()) {
+		$("#password2").focus().after(alertify.error("Las contraseñas no coinciden"));
+		return false;
 	}
+	Actualizar();
 
 });
 
 $(document).ready(function() {
-  Cargar();
-
-//Validacion de formulario
-	var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
-	    $("#btnActualizar").click(function (){
-	        $(".error").remove();
-	        if( $("#username").val() == "" ){
-	            $("#username").focus().after(alertify.error("Ingresa un nombre de usuario"));
-	            return false;
-	        }else if( $("#email").val() == "" || !emailreg.test($("#email").val()) ){
-	            $("#email").focus().after(alertify.error("Ingresa un correo valido"));
-	            return false;
-	        }else if( $("#password").val() == ""){
-	            $("#password").focus().after(alertify.error("Ingresa una contraseña"));
-	            return false;
-					}else if ($('#password').val() != $('#password2').val()) {
-							$("#password2").focus().after(alertify.error("Las contraseñas no coinciden"));
-					}
-			});
+	Cargar();
 });
